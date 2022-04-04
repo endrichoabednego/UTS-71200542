5d023466b688cf3ed57f0a96f5e8f93c5d2ce5b8
Endricho Abednego <78333206+endrichoabednego@users.noreply.github.com>
2022-04-04 16:17:15 +0700

    Add files via upload

---
+++ TTS-71200542/Vehicle/src/main/java/com/rplbo/Wheel.java
@@ -0,0 +1,21 @@
+package com.rplbo;
+
+public class Wheel {
+    private int width;
+    private int diameter;
+
+    public Wheel(int width, int diameter){
+        this.width=width;
+        this.diameter=diameter;
+    }
+
+    public int getDiameter() {
+        return diameter;
+    }
+
+    public int getwidth() {
+        return width;
+    }
+
+
+    }
